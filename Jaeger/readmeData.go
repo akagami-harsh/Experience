c4a2131e5e3389d95f7aa116b1572a93b47213e3
Harshvir Potpose <hpotpose62@gmail.com>
2024-01-25 21:25:26 +0530

    generate new pull request data
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>

--- Jaeger/readmeData.go
+++ Jaeger/readmeData.go
@@ -6,14 +6,14 @@ const Data = `
 - Name: Harshvir Potpose
 - Email: <hpotpose62@gmail.com>
 - GitHub Username: [akagami-harsh
-](https: //github.com/akagami-harsh)
+](https://github.com/akagami-harsh)
 
 ## About the project
 
 Jaeger is an open-source tool that monitors and troubleshoots distributed systems based on microservices architecture. It was built by Uber and open-sourced in 2015.
 
 More information on the projects can be found on the [JaegerTracing Website
-](https: //www.jaegertracing.io/).
+](https://www.jaegertracing.io/).
 
 ## Technologies used
 
@@ -28,8 +28,8 @@ More information on the projects can be found on the [JaegerTracing Website
 ## Technical Contributions
 
 ### [Jaeger
-](https: //github.com/jaegertracing/jaeger)
+](https://github.com/jaegertracing/jaeger)
 
 View all pull requests by me at a glance : [VIEW ALL PULL REQUESTS
-](https: //github.com/jaegertracing/jaeger/pulls?q=is%3Apr+author%3Aakagami-harsh+is%3Aclosed)
+](https://github.com/jaegertracing/jaeger/pulls?q=is%3Apr+author%3Aakagami-harsh+is%3Aclosed)
 `
